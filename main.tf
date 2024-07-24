10c214a622172ff2c8131af6f200506ae2508755
sermartin79 <sermartin79@gmail.com>
2024-07-24 10:15:53 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ data "aws_s3_object" "base" {
 resource "aws_instance" "web" {
     ami           = data.aws_ami.amazon_linux_2.id
     instance_type = "t3a.micro"
-    subnet_id     = data.aws_s3_object.outputs.public_subnets.value[0]
+    subnet_id     = data.aws_s3_object.base.outputs.public_subnets[0].value[0]
 
     tags = {
         Name = "instancia-tonta"
