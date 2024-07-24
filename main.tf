d1779d33d08a30fe3b645aac94997971ba50a50f
Javier Hernández <79507700+javierhstr@users.noreply.github.com>
2024-07-24 11:42:55 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,17 +39,26 @@ data "aws_s3_object" "terraform_state" {
   key    = "aws_vpc_tonta/Structuralia/dev/vptonta/terraform.tfstate"
 }
 
-debug "Retrieved data", data.aws_s3_object.terraform_state.body
-
+# Verify that the file content is not null
 locals {
-  state_outputs = jsondecode(data.aws_s3_object.terraform_state.body)["outputs"]
+  # Debug: Print the state file content
+  debug_state_content = "${data.aws_s3_object.terraform_state.body}"
+
+  tfstate_content = data.aws_s3_object.terraform_state.body != null && data.aws_s3_object.terraform_state.body != "" ? data.aws_s3_object.terraform_state.body : "{}"
+  tfstate        = jsondecode(coalesce(tfstate_content, "{}"))
 }
 
+# ---------------------------------------------------------
+# AWS instance resource
+# ---------------------------------------------------------
 resource "aws_instance" "web" {
   ami           = data.aws_ami.amazon_linux_2.id
   instance_type = "t3a.micro"
-  subnet_id     = local.state_outputs["public_subnets"].value[0]
-  tags         = {
+  subnet_id     = local.tfstate.outputs.public_subnets.value[0]
+
+  tags = {
     Name = "instancia-tonta"
   }
-}
+
+  # Debug: Print the instance subnet ID
+  debug_subnet_id = "${local.tfstate.outputs.public_subnets.value[0]}"
